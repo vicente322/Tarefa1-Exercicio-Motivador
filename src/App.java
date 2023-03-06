a4e72a7d62f8430827d0829c2c574935c96f37f0
vicente322 <vicentehofmeister@gmail.com>
2023-03-06 18:52:58 -0300

    Exercicio 1 feito

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA, 10, 55);
+        Carro basico = new Carro("Basico", TipoCombustivel.GASOLINA,
+        10, 55);
+        Carro esportivo = new Carro("Esportivo", TipoCombustivel.GASOLINA,
+        6, 45);
 
         System.out.println("Tipos de veiculos:");
-        System.out.println(basico);
 
-        System.out.println("\n\n----------------");
+        System.out.println(basico);
         System.out.println("\nAbastencendo carro basico com gasolina");
         basico.abastece(TipoCombustivel.GASOLINA, 55);
         System.out.println(basico);
@@ -14,5 +16,17 @@ public class App {
         basico.viaja(250);
         basico.viaja(150);
         System.out.println(basico);
+
+        System.out.println("\n\n----------------\n\n");
+
+        System.out.println(esportivo);
+        System.out.println("\nAbastencendo carro esportivo com gasolina");
+        esportivo.abastece(TipoCombustivel.GASOLINA, 45);
+        System.out.println(esportivo);
+        System.out.println("\nViajando com o carro esportivo");
+        esportivo.viaja(250);
+        esportivo.viaja(150);
+        System.out.println(esportivo);
+
     }
 }
